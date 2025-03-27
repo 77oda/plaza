0b5e6f46a9efdbc3627e81a142480ec562dfb1b2
7oda <mahmoud.abdo2772@gmail.com>
2025-03-28 00:24:43 +0200

    title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'plaza 22',
+      title: 'plaza',
       theme: ThemeData(
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
